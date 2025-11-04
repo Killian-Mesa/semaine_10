2319750712f44d03c61f57a9b026b2735700ae14
Killian-Mesa <6321614@cegepmontpetit.ca>
2025-11-04 17:15:49 -0500

    bruh

---
+++ ex_3.py
@@ -0,0 +1,10 @@
+info = {
+    "lieu": "Raccon City",
+    "date": "2025-07-12",
+    "temperatures_C": {
+        "matin": 18.2,
+        "midi": 22.5,
+        "apres_midi": 24.1,
+        "soir": 20.3
+    }
+}
